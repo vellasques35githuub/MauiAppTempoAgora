e54b6e4f75f6cf2dd3a59fce94f59eac32a60f72
fabio <fabio@DESKTOP-C274NH0>
2025-04-06 05:02:42 -0300

    Montagem e alteração do sistema de tempo.

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,25 +1,56 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System;
+using System.Diagnostics;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                if(t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n " +
+                                         $"Nascer do Sol {t.sunrise} \n" +
+                                         $"Por do Sol {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Velocidade do Vento: {t.speed} \n" +
+                                         $"Visibilidade: {t.visibility} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+                    } else
+                    {
+                        
+                        lbl_res.Text = "Cidade não encontrada";
+                    }
+
+                }else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
-
 }
